a66eba0325742890569bad4de6b4922e162c1f84
jjamadagni_AltranPT <43004345+jjamadagni@users.noreply.github.com>
2018-09-07 17:34:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,43 +26,20 @@
 XERCES_CPP_NAMESPACE_USE
 using namespace std;
  
- int64_t getCurrentTime()
-{
-    struct timeval tval;
-    gettimeofday(&tval, NULL);
-    return tval.tv_sec * 1000000LL + tval.tv_usec;
-}
-
  
- struct name {
-              string filename;
-              string stringMember;
-              string intmember;
-       };
+ void ValidateSchema(const char* schemaFilePath, const char* xmlFilePath); 
+ int xml(string *filename,string *stringMember,string *intmember);
+ void json();
+ 
+ 
+ struct name 
+	{
+        string filename;
+        string stringMember;
+        string intmember;
+    };
   name a[3];
 
- 
- class WStr
-{
-    private:
-        XMLCh*  wStr;
- 
-    public:
-        WStr(const char* str)
-        {
-            wStr = XMLString::transcode(str);
-        }
- 
-        ~WStr()
-        {
-            XMLString::release(&wStr);
-        }
- 
-        operator const XMLCh*() const
-        {
-            return wStr;
-        }
-};
  
  class ParserErrorHandler : public ErrorHandler
 {
@@ -99,101 +76,51 @@ using namespace std;
  
 int main(int argc, const char *argv[]) 
 {
-	
-	   void ValidateSchema(const char* schemaFilePath, const char* xmlFilePath);
-       int xml(string *filename,string *stringMember,string *intmember);
-	   //Reading the xsd file
-	   string line;
-     ifstream myfile ("XSD.txt");
-     if (myfile.is_open())
-     {
-      while ( getline (myfile,line) )
-     {
-      cout << line << '\n';
-     }
-       myfile.close();
-     }
+	   
+	   
+	   
+	  char function;
+	  char i;
+	  
 
-     else cout << "Unable to open file"; 
-	   
-	   //copying the contents of the file in to an array
-	   
-	   
-	    std::vector<std::string> NFAArray;
-	    std::string line_2;
-	    std::string str2 ("STATES");
-	    std::ifstream myFile("XSD.txt");
-			if (!myFile)
+	  std::cout << "Please select the process you want the app to perform"  << std::endl;
+	  std::cout << "1. For XML Genereation" << std::endl;
+	  std::cout << "2. For JASON export" <<std::endl;
+	  std::cout << "3. To exit the program" << std::endl;
+	  
+	bool repeat(true);
+	  do{   
+	        cin >> function;
+			switch (function)
 			{
-			std::cout << "Error opening output file" << std::endl;
-			system("pause");
-			return -1;
-			}
-			while (std::getline(myFile, line))
-		{
-			(NFAArray).push_back(line);
-		}
-
-		std::vector<std::string> states;
-		int num_lines = (NFAArray).size();// sizeof(int);
-		//cout << num_lines << endl;
-		
-	   int function;
-	   int i;
-	   //std::cout << "Please select the process you want the app to perform"  << std::endl;
-	   //std::cout << "A. For XML Genereation" << std::endl;
-	   //std::cout << "B. For JASON export" <<std::endl;
-	  // std::cout << "C. To exit the program" << std::endl;
-	   
-	   //cin >> i ;
-	   
-      
-	  
-	  
-       a[0].filename = "data4.xml";
-       a[0].stringMember = "str1";
-       a[0].intmember = "1";
-       
-	   
-        
-      
-       xml(&a[0].filename, &a[0].stringMember, &a[0].intmember);
-       
-	  
-    
-       string xml_str, json_str;
-       ifstream inf;
-       ofstream outf;
-       ostringstream oss;
-       char BOM[4] = {(char)0xEF, (char)0xBB, (char)0xBF, '\0'}; /*BOM String*/
-       int64_t start_time, end_time;
-
-       inf.open("data3.xml");
-       outf.open("data1.js.txt");
-       oss.str("");
-       oss << inf.rdbuf();
-       xml_str = oss.str();
-       inf.close();
-       start_time = getCurrentTime();
-       json_str = xml2json(xml_str.c_str());
-       end_time = getCurrentTime();
-       cout << "time1: " << end_time - start_time << endl;
-       outf << BOM << json_str;
-       outf.close();
-
-	   
-       
-	   
-	   
-	   
-	   //Validator
-	   
-	XMLPlatformUtils::Initialize();
- 
-    ValidateSchema(argv[1], argv[2]);
- 
-    XMLPlatformUtils::Terminate();
+				case '1':
+				xml(&a[0].filename, &a[0].stringMember, &a[0].intmember);
+				cout << "xml is created" << endl;
+                
+				//Validator	
+				XMLPlatformUtils::Initialize();  
+				ValidateSchema(argv[1], "data2.xml");   
+				XMLPlatformUtils::Terminate();
+				repeat = false;
+				break;
 	
+				case '2':
+				json();
+				cout << "xml is exported to json" << endl;
+				repeat = false;
+				break;
+		
+				case '3':
+				exit(0);
+				repeat = false;
+				break;
+		
+				default:
+				cout << "Please enter a valid option, 1, 2 or 3" << endl;
+				repeat = true;
+				break;
+			}
+	    }while(repeat);
 	return 0;
 	
  
@@ -201,8 +128,11 @@ int main(int argc, const char *argv[])
        
 int xml(string *filename,string *stringMember,string *intmember){
     
+	a[0].filename = "data2.xml";
+    a[0].stringMember = "str1";
+    a[0].intmember = "1";
 	
-	TiXmlDocument doc;
+	TiXmlDocument doc  = TiXmlDocument();
 	TiXmlDeclaration* decl = new TiXmlDeclaration("1.0", "", "");
 	doc.LinkEndChild(decl);
 
@@ -223,36 +153,56 @@ int xml(string *filename,string *stringMember,string *intmember){
 
 	doc.SaveFile(filename->c_str());
     
+	   
 	return 0;
 }
 
 
 void ValidateSchema(const char* schemaFilePath, const char* xmlFilePath)
-{ 
+{  
    
     XercesDOMParser domParser;
+	cout << "hello" << endl;
     if (domParser.loadGrammar(schemaFilePath, Grammar::SchemaGrammarType) == NULL)
     {
-        fprintf(stderr, "couldn't load schema\n");
+        fprintf(stderr, "couldn't load schema OR XML file doesn't conform to the schema\n");
         return;
     }
-
+   
+   cout << "hello1" << endl;
    ParserErrorHandler parserErrorHandler;
- 
-    domParser.setErrorHandler(&parserErrorHandler);
-    domParser.setValidationScheme(XercesDOMParser::Val_Auto);
-    domParser.setDoNamespaces(true);
-    domParser.setDoSchema(true);
-    domParser.setValidationConstraintFatal(true);
- 
-    domParser.parse(xmlFilePath);
+   
+   domParser.setErrorHandler(&parserErrorHandler);
+   domParser.setValidationScheme(XercesDOMParser::Val_Auto);
+   domParser.setDoNamespaces(true);
+   domParser.setDoSchema(true);
+   domParser.setValidationConstraintFatal(true);
+   
+   domParser.parse(xmlFilePath);
     if (domParser.getErrorCount() == 0)
-        printf("XML file validated against the schema successfully\n");
+		printf("XML file validated against the schema successfully\n");
     else
         printf("XML file doesn't conform to the schema\n");
-
-	  
+	
 }
+void json() 
+{ 
+    string xml_str, json_str;
+    ifstream inf;
+    ofstream outf;
+    ostringstream oss;
+    char BOM[4] = {(char)0xEF, (char)0xBB, (char)0xBF, '\0'}; /*BOM String*/
+      
 
+    inf.open("data1.xml");
+    outf.open("data2.js.txt");
+    oss.str("");
+    oss << inf.rdbuf();
+    xml_str = oss.str();
+    inf.close();
+    json_str = xml2json(xml_str.c_str());
+    outf << BOM << json_str;
+    outf.close();	   
+}
 
        
